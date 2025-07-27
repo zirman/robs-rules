8fe0013472e26331a9e3da1cd3bf6a8766fb1606
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-07-26 21:17:08 -0400

    Configured detekt in convention plugin

--- buildSrc/src/main/kotlin/buildsrc/convention/detekt-rules.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/detekt-rules.gradle.kts
@@ -2,8 +2,9 @@
 
 package buildsrc.convention
 
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import org.gradle.accessors.dm.LibrariesForLibs
-import org.gradle.kotlin.dsl.the
 
 plugins {
     kotlin("jvm")
@@ -11,15 +12,50 @@ plugins {
     `maven-publish`
 }
 val libs = the<LibrariesForLibs>()
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults.
+    allRules = false // activate all available (even unstable) rules.
+    autoCorrect = false // To enable or disable auto formatting.
+    // To enable or disable parallel execution of detekt on multiple submodules.
+    parallel = true
+    // point to your custom config defining rules to run, overwriting default behavior.
+    config.setFrom("${rootProject.projectDir}/detekt.yml")
+}
 dependencies {
     compileOnly(libs.detektApi)
     compileOnly(libs.detektTooling)
+    detektPlugins(libs.detektFormatting)
     detektPlugins(libs.detektRulesRuleauthors)
     testImplementation(libs.assertjCore)
     testImplementation(libs.detektTest)
     testImplementation(libs.junitJupiter)
     testRuntimeOnly(libs.junitPlatformEngine)
     testRuntimeOnly(libs.junitPlatformLauncher)
+}
+tasks.withType<Detekt> {
+    config.setFrom(files("${rootProject.projectDir}/detekt.yml"))
+    reports {
+        // observe findings in your browser with structure and code snippets
+        html {
+            required = true
+            outputLocation = file("build/reports/mydetekt.html")
+        }
+        // similar to the console output, contains issue signature to manually edit baseline files
+        txt {
+            required = true
+            outputLocation = file("build/reports/mydetekt.txt")
+        }
+        // simple Markdown format
+        md {
+            required = true
+            outputLocation = file("build/reports/mydetekt.md")
+        }
+    }
+    jvmTarget = libs.versions.jvmTarget.get()
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    config.setFrom(files("${rootProject.projectDir}/detekt.yml"))
+    jvmTarget = libs.versions.jvmTarget.get()
 }
 tasks.named<Test>("test") {
     useJUnitPlatform()
